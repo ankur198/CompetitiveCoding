4f7c2f67deeaff6700c8e3613589b316cf63e774
Ankur Nigam <ankur.nigam198@gmail.com>
2019-06-28 22:02:14 +0530

    Philaland Coin

---
+++ Codevita/2019/Round1/Zone1/PhilalandCoin/Coin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhilalandCoin
+{
+    class SelectedCoins
+    {
+        public int Index { get; set; }
+        public List<int> Coins { get; set; }
+
+        public SelectedCoins()
+        {
+            Coins = new List<int>();
+        }
+
+        public int BuyDifference(int n)
+        {
+            minDifference = Int32.MaxValue;
+            traverse(0, new List<int>(), n);
+            return minDifference;
+        }
+
+        int minDifference;
+
+        void traverse(int index, List<int> currentCoins, int remDifference)
+        {
+            if (remDifference < minDifference && remDifference > -1)
+            {
+                minDifference = remDifference;
+            }
+            if (remDifference > -1 && index < Coins.Count)
+            {
+                var c1 = new List<int>(currentCoins);
+                var c2 = new List<int>(currentCoins);
+                var value = Coins[index];
+                c1.Add(value);
+                index++;
+
+                traverse(index, c1, remDifference - value);
+                traverse(index, c2, remDifference);
+            }
+        }
+    }
+
+    class Builder
+    {
+        public List<int> MinimizeCoins(int maxValue)
+        {
+            SelectedCoins node = new SelectedCoins();
+            int coin = 1;
+            for (int i = 1; i < maxValue+1; i++)
+            {
+                var diff = node.BuyDifference(i);
+                if (diff > 0)
+                {
+                    node.Coins.Add(coin++);
+                }
+            }
+
+            return node.Coins;
+        }
+    }
+}
---
+++ Codevita/2019/Round1/Zone1/PhilalandCoin/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PhilalandCoin
+{
+    class Program
+    {
+        static void MainMy(string[] args)
+        {
+            Builder builder = new Builder();
+            var x = builder.MinimizeCoins(16);
+            //foreach (var item in x)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            Console.WriteLine(x.Count);
+        }
+        static void Main(string[] args)
+        {
+            var cases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                Builder builder = new Builder();
+                var x = builder.MinimizeCoins(int.Parse(Console.ReadLine()));
+                Console.WriteLine(x.Count);
+            }
+        }
+    }
+}
